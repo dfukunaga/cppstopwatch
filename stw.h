068180d1b74ed299ca68fb2a6b1545f8a649e548
dfukunaga <dfukunaga.jp@gmail.com>
2016-06-13 23:22:07 +0900

    Implement stopwatch

--- stw.h
+++ stw.h
@@ -1,55 +1,115 @@
 #ifndef CPPSTOPWATCH_STW_H_
 #define CPPSTOPWATCH_STW_H_
 
+#include <chrono>
 #include <memory>
+#include <stdexcept>
 
 namespace stw {
 
-class Stopwatch;
-
-std::unique_ptr<Stopwatch> CreateStarted() {
-  return nullptr;
-}
-
-std::unique_ptr<Stopwatch> CreateUnstarted() {
-  return nullptr;
-}
+using nano = std::chrono::nanoseconds;
+using micro = std::chrono::microseconds;
+using milli = std::chrono::milliseconds;
+using sec = std::chrono::seconds;
+using min = std::chrono::minutes;
+using hour = std::chrono::hours;
 
 class Stopwatch {
  public:
-  Stopwatch& Start() {
-    return *this;
+  using Rep = long long int;
+  using Clock = std::chrono::high_resolution_clock;
+
+  Stopwatch() noexcept : running_(false), elapsed_time_(0) {}
+
+  virtual ~Stopwatch() {}
+  
+  Stopwatch* Start() {
+    if (running_) {
+      throw std::runtime_error("Stopwatch has already started");
+    }
+
+    start_time_ = Clock::now();
+    running_ = true;
+    return this;
   }
 
-  Stopwatch& Stop() {
-    return *this;
+  Stopwatch* Stop() {
+    if (!running_) {
+      throw std::runtime_error("Stopwatch has not yet started");
+    }
+
+    elapsed_time_ += Clock::now() - start_time_;
+    running_ = false;
+    return this;
   }
 
-  Stopwatch& Reset() {
-    return *this;
+  Stopwatch* Reset() noexcept {
+    elapsed_time_ = decltype(elapsed_time_)::zero();
+    running_ = false;
+    return this;
   }
 
   template <class TimeUnit>
-  long Elapsed() {
-    return 0;
+  Rep Elapsed() const noexcept {
+    auto time = elapsed_time_;
+    if (running_) {
+      time += Clock::now() - start_time_;
+    }
+    return std::chrono::duration_cast<TimeUnit>(time).count();
   }
 
-  bool IsRunning() {
-    return true;
+  bool IsRunning() const noexcept {
+    return running_;
   }
+
+ protected:
+  bool running_;
+  std::chrono::time_point<Clock> start_time_;
+  std::chrono::duration<Rep, std::nano> elapsed_time_;
 };
+
+std::unique_ptr<Stopwatch> CreateStarted() noexcept {
+  std::unique_ptr<Stopwatch> sw(new Stopwatch);
+  sw->Start();
+  return sw;
+}
+
+std::unique_ptr<Stopwatch> CreateUnstarted() noexcept {
+  return std::unique_ptr<Stopwatch>(new Stopwatch);
+}
 
 } // namespace stw
 
 namespace std {
 
 template <class E, class T>
-basic_ostream<E, T>& operator<<(basic_ostream<E, T>& os,
-                                const stw::Stopwatch &sw);
+basic_ostream<E, T>& operator<<(basic_ostream<E, T> &os,
+                                const stw::Stopwatch &sw) {
+  auto time = sw.Elapsed<stw::nano>();
+  if (time < 1000) {
+    os << time << " ns";
+  } else if (time < 1000 * 1000) {
+    os << time / 1000.0 << " µs";
+  } else if (time < 1000 * 1000 * 1000) {
+    os << time / 1000.0 / 1000.0 << " ms";
+  } else {
+    os << time / 1000.0 / 1000.0 / 1000.0 << " s";
+  }
+  
+  return os;
+}
 
 template <class E, class T>
-basic_ostream<E, T>& operator<<(basic_ostream<E, T>& os,
-                                const shared_ptr<stw::Stopwatch> &sw);
+basic_ostream<E, T>& operator<<(basic_ostream<E, T> &os,
+                                const unique_ptr<stw::Stopwatch> &sw) {
+  return os << *sw.get();
+}
+
+template <class E, class T>
+basic_ostream<E, T>& operator<<(basic_ostream<E, T> &os,
+                                const shared_ptr<stw::Stopwatch> &sw) {
+  return os << *sw.get();
+}
 
 } // namespace std
 
